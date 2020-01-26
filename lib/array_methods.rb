296a52e8327ff7597af1d222d48718496a30b93d
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-01-26 08:38:08 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,29 @@
 def using_include(array, element)
   return array.include?(element)
+  return array 
 end
 
 def using_sort(array)
   array.sort
+  return array 
 end
 
 def using_reverse(array)
   array.reverse
+  return array 
 end
 
 def using_first(array)
   array.first
+  return array 
 end
 
 def using_last(array)
   array.last
+  return array
 end
 
 def using_size(array)
   array.size
+  return array 
 end
